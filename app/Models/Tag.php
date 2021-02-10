2f6573b4229aa015b257b59c391f75b77cc573f8
Ramunas <rambek@gmail.com>
2021-02-10 17:01:06 +0200

    DB connections

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Tag extends Model
 {
     use HasFactory;
+    use Sluggable;
+
+    public function posts()
+    {
+        return $this->belongsToMany(
+            Post::class,
+            'posts_tags',
+            'tag_id',
+            'post_id'
+        );
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }
